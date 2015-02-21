780982983df1414b9497fc600324f2220886dd44
roadscape <roadscape@users.noreply.github.com>
2015-02-20 22:03:51 -0600

    removed tx logic from wallet class

--- app/models/tx.rb
+++ app/models/tx.rb
@@ -1,4 +1,55 @@
 class Tx < ActiveRecord::Base
   validates_presence_of     :block_num, :trx_id, :json
   validates_numericality_of :block_num
+
+  # Given a single transaction-row from the API, 
+  #   make sure we have it saved, and
+  #   return it in a Tx object wrapper
+  def self.cache(tx_data)
+    Tx.find_by_trx_id(tx_data['trx_id']) || Tx.create(
+       :block_num => tx_data['block_num'],
+       :trx_id    => tx_data['trx_id'],
+       :json      => tx_data.to_json)
+  end
+
+  def raw
+    @raw ||= JSON::parse(json)
+  end
+
+  def raw=(hash)
+    @raw = hash
+    self.json = hash.to_json
+  end
+
+  def ledger_entries
+    tx = raw
+    raise "UNHANDLED CASE" unless tx.delete("is_market")        == false
+    raise "UNHANDLED CASE" unless tx.delete("is_market_cancel") == false
+    raise "UNHANDLED CASE" unless tx.delete("is_virtual")       == false
+    raise "UNHANDLED CASE" unless tx.delete("error")            == nil
+    raise "UNHANDLED CASE" unless tx.delete("is_confirmed")     == true
+
+    tx['date']    = bts_date(tx.delete('timestamp'))
+    tx['expires'] = bts_date(tx.delete('expiration_timestamp'))
+    tx['fee']     = tx['fee']
+    entries       = tx.delete("ledger_entries")
+    unhandled     = tx.keys - %w{trx_id block_num date expires fee}
+    raise "Unhandled keys: #{unhandled.join(', ')} -- #{tx.inspect}" if unhandled.any?
+    raise "Multiple ledger entries" if entries.size != 1
+
+    out = []
+    entries.each do |e|
+      e.delete('running_balances')
+      e['from'] = e.delete('from_account')
+      e['to']   = e.delete('to_account')
+      out << tx.merge(e) #unless e['from'] == e['to']
+    end
+    out
+  end
+
+private
+
+  def bts_date ts
+    DateTime.parse(ts).to_time.localtime("-06:00").strftime("%Y-%m-%d %H:%M:%S")
+  end
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -15,47 +15,11 @@ class Wallet
 
   def self.new_entries
     out = []
-    BitShares::API::Wallet.account_transaction_history.each do |tx|
-      #next if Tx.find_by_trx_id(tx['trx_id'])
-      Tx.find_by_trx_id(tx['trx_id']) || Tx.create(
-       :block_num => tx['block_num'], 
-       :trx_id    => tx['trx_id'],
-       :json      => tx.to_json)
-
-      out += Wallet.clean_tx(tx).select{|f| f['to'] == ACCT && f['from'] != ACCT}
+    BitShares::API::Wallet.account_transaction_history.each do |tx_data|
+      tx = Tx.cache(tx_data)
+      out += tx.ledger_entries.select{|f| f['to'] == ACCT && f['from'] != ACCT}
     end
     out
-  end
-
-  def self.clean_tx tx
-    raise "UNHANDLED CASE" unless tx.delete("is_market")        == false
-    raise "UNHANDLED CASE" unless tx.delete("is_market_cancel") == false
-    raise "UNHANDLED CASE" unless tx.delete("is_virtual")       == false
-    raise "UNHANDLED CASE" unless tx.delete("error")            == nil
-    raise "UNHANDLED CASE" unless tx.delete("is_confirmed")     == true
-
-    tx['date']    = bts_date(tx.delete('timestamp'))
-    tx['expires'] = bts_date(tx.delete('expiration_timestamp'))
-    tx['fee']     = tx['fee']
-    entries       = tx.delete("ledger_entries")
-    unhandled     = tx.keys - %w{trx_id block_num date expires fee}
-    raise "Unhandled keys: #{unhandled.join(', ')} -- #{tx.inspect}" if unhandled.any?
-    raise "Multiple ledger entries" if entries.size != 1
-
-    out = []
-    entries.each do |e|
-      e.delete('running_balances')
-      e['from'] = e.delete('from_account')
-      e['to']   = e.delete('to_account')
-      out << tx.merge(e) #unless e['from'] == e['to']
-    end
-    out
-  end
-
-private
-
-  def self.bts_date ts
-    DateTime.parse(ts).to_time.localtime("-06:00").strftime("%Y-%m-%d %H:%M:%S")
   end
 
 end
