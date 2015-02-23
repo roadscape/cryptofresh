19705a50be09965b1d73134f864370439ceb502c
roadscape <roadscape@users.noreply.github.com>
2015-02-23 11:38:54 -0600

    streamline matching routine

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -3,23 +3,23 @@ class Wallet
 
   # Checks all open orders vs all incoming payments
   def self.scan
+    # Get all ledger entries that transfer to our cashier account
+    entries = []
+    BitShares::API::Wallet.account_transaction_history.each do |tx_data|
+      tx = Tx.cache(tx_data)
+      entries += tx.ledger_entries.select{|f| f['to'] == ACCT && f['from'] != ACCT}
+    end
+
+    # For each tx, find the matching unpaid order, and check it pays in full
     open_orders = Order.where(:paid_at => nil).where('due_at > ?', 2.hours.ago)
-    Wallet.new_entries.each do |tx|
+    entries.each do |tx|
       order = open_orders.find{|o| tx['memo'].include?(o.pub_id)}
-      next unless order && !order.paid_at
+      next unless order
       next unless Amount.from_bts(tx['amount']).includes?(order.amount)
       order.update_attributes(:paid_at => DateTime.now, :trx_id => tx['trx_id'])
     end
   end
 
 
-  def self.new_entries
-    out = []
-    BitShares::API::Wallet.account_transaction_history.each do |tx_data|
-      tx = Tx.cache(tx_data)
-      out += tx.ledger_entries.select{|f| f['to'] == ACCT && f['from'] != ACCT}
-    end
-    out
-  end
 
 end
